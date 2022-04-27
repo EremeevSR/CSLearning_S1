9928e209710ef90718365578cf0fc0513fad52d7
Sergey_415 <sergey123.3415@gmail.com>
2022-04-27 21:34:48 +0500

    Код решения задачи №1

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 1
+Напишите программу, которая на вход
+принимает два числа и выдаёт, какое число большее, а
+какое меньшее. Например: a = 5; b = 7 -> max = 7.
+*/
+
+//При решении задачи условимся на
+//интерактивный ввод целочисленных значений.
+Console.WriteLine("Введите два числа:");
+int firstVar = Convert.ToInt32(Console.ReadLine());
+int secondVar = Convert.ToInt32(Console.ReadLine());
+if (firstVar > secondVar) {
+    Console.Write("max = ");
+    Console.WriteLine(firstVar);
+}
+else {
+    Console.Write("max = ");
+    Console.WriteLine(secondVar);
+}
