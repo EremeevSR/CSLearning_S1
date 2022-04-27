24571e15c129f4f4513fb672c4de226ce25b4801
Sergey_415 <sergey123.3415@gmail.com>
2022-04-27 21:48:46 +0500

    Код решения задачи №2

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая принимает на
+вход три числа и выдаёт максимальное из этих чисел.
+Например: 2, 3, 7 -> 7 */
+
+//Условимся на интерактивный ввод трех целочисленных значений.
+int [] numbers = new int[3];
+int max = 0;
+Console.WriteLine("Введите три числа:");
+for (int i = 0; i < 3; i++){
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+    if (max < numbers[i]) max = numbers[i];
+}
+Console.WriteLine("Max = " + Convert.ToString(max));
